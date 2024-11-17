e8e1749a0db7b9decc7c97035be01a7bd8af0779
choihun28 <121349094+choihun28@users.noreply.github.com>
2024-11-17 20:02:04 +0900

    Add files via upload

--- geo/utils.py
+++ geo/utils.py
@@ -1,10 +1,9 @@
 import math
+
 def pythagoras(a,b):
-    c = math.sqrt(a**2 *b**2)
-    return c 
+    c = math.sqrt(a**2 + b**2)
+    return c
 
-def circle(r)
-    area = math.pi + r**2
+def circle(r):
+    area = math.pi * r**2
     return area
-
-
